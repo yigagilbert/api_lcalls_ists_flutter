87796b0cf868bffbdc96cc968bb7744b77152846
Yiga Gilbert <gilbert.yiga@students.mak.ac.ug>
2022-09-14 18:11:21 +0300

    intial commit
    first commit

---
+++ android/app/src/main/kotlin/com/example/api_calls_lists_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.api_calls_lists_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:api_calls_lists_flutter/api_call.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late Future<List<Users>> users;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    users = getUsers();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text('Api Calls in Flutter'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: FutureBuilder<List<Users>>(
+            future: users,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return ListView.builder(
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(snapshot.data![index].name),
+                        subtitle: Text(snapshot.data![index].email),
+                        trailing: Text(snapshot.data![index].username),
+                      );
+                    });
+              } else if (snapshot.hasError) {
+                return Text(snapshot.error.toString());
+              } else {
+                return CircularProgressIndicator();
+              }
+            }),
+      ),
+    ));
+  }
+}
